66373fd66b503b47d17d8550748ea9be1fcc635e
Joe Ryan <joe@Joes-iMac.local>
2021-09-05 22:15:53 +0100

    add utterances

--- src/pages/{mdx.slug}.js
+++ src/pages/{mdx.slug}.js
@@ -6,13 +6,35 @@ import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPost = ({ data }) => {
     const { site } = useSiteMetadata()
+    React.useEffect(() => {
+        const script = document.createElement('script')
+        const anchor = document.getElementById('comments')
+        script.setAttribute('src', 'https://utteranc.es/client.js')
+        script.setAttribute('crossorigin', 'anonymous')
+        script.setAttribute('async', true)
+        script.setAttribute('repo', '000dry/joe-ryan.io-project')
+        script.setAttribute('issue-term', data.mdx.frontmatter.title)
+        script.setAttribute('theme', 'preferred-color-scheme')
+        anchor.appendChild(script)
+        return () => {
+          anchor.innerHTML = ''
+        }
+      })
+
     return (
-        <Layout pageTitle={data.mdx.frontmatter.title} title={`${data.mdx.frontmatter.title} | ${site.siteMetadata.title}`}>
-            <p>{data.mdx.frontmatter.date}</p>
-            <MDXRenderer>
-                {data.mdx.body}
-            </MDXRenderer>
-        </Layout>
+        <>
+            <Layout pageTitle={data.mdx.frontmatter.title} title={`${data.mdx.frontmatter.title} | ${site.siteMetadata.title}`}>
+                <p>{data.mdx.frontmatter.date}</p>
+                <MDXRenderer>
+                    {data.mdx.body}
+                </MDXRenderer>
+                <h3>Sign in with GitHub to leave a comment.</h3>
+                <div
+                    id="comments"
+                >
+                </div>
+            </Layout>
+        </>
     )
 }
 
